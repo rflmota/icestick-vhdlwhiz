163f9a93b07b23f5fd82f2b34cdaa0ac87ab99d2
Jonas Julian Jensen <jojuljen@gmail.com>
2020-01-17 18:19:29 +0700

    Generate the clock signal using WAIT FOR

--- top_tb.vhd
+++ top_tb.vhd
@@ -7,8 +7,19 @@ end top_tb;
 
 architecture sim of top_tb is
 
+  constant clk_hz : integer := 12e6;
+  constant clk_period : time := 1 sec / clk_hz;
+
   signal clk : std_logic := '1';
 
 begin
+
+  CLOCK_PROC : process
+  begin
+
+    wait for clk_period / 2;
+    clk <= not clk;
+
+  end process;
 
 end architecture;
