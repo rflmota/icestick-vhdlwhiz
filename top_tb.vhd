a52d3b9a1d928bc4b5b8b8ef91b293774160e1dd
Jonas Julian Jensen <jojuljen@gmail.com>
2020-01-17 18:23:52 +0700

    LOOP, WHILE-loop, FOR-loop, and WAIT UNTIL

--- top_tb.vhd
+++ top_tb.vhd
@@ -15,8 +15,6 @@ architecture sim of top_tb is
   signal segments : std_logic_vector(6 downto 0);
   signal digit_sel : std_logic;
 
-  signal sig : integer := 0;
-
 begin
 
   -- Device under test
@@ -39,31 +37,13 @@ begin
   RESET_PROC : process
   begin
 
-    wait for 10 ns;
+    for count in 1 to 5 loop
+      wait until clk = '1';
+    end loop;
+
     rst_n <= '1';
     wait;
 
-  end process;
-
-  TEMP_PROC : process
-    variable var : integer := 0;
-  begin
-
-    var := 0;
-    sig <= 0;
-
-    wait for 10 ns;
-
-    var := var + 1;
-    sig <= sig + 1;
-
-    var := var + 1;
-    sig <= sig + 1;
-
-
-
-
-    wait;
   end process;
 
 end architecture;
