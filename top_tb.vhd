05f7fa8af96da42fafc11cd113842b20fc49cc7e
Jonas Julian Jensen <jojuljen@gmail.com>
2020-01-17 18:18:16 +0700

    Create a testbench and simulate in ModelSim

---
+++ top_tb.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity top_tb is
+end top_tb;
+
+architecture sim of top_tb is
+
+  signal clk : std_logic := '1';
+
+begin
+
+end architecture;
